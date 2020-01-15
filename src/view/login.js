71de66a1366d8fa610255bfb80712245329ee1dc
KAREN RAMIREZ <karen.bussines@hotmail.com>
2020-01-15 10:55:02 -0600

    se implementa el SPA

---
+++ src/view/login.js
@@ -0,0 +1,18 @@
+export default () =>{
+    const viewLogin = `
+    
+    <h2><strong>HOLAAAA....</strong></h2>
+    
+    `
+    const divElement = document.createElement ('div');
+    divElement.innerHTML = viewLogin;
+    return divElement;
+}
+
+
+
+
+
+
+
+
