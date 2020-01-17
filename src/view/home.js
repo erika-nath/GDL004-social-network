40c1d923d9d94d1a2ca958fd6c306a7e95669629
KAREN RAMIREZ <karen.bussines@hotmail.com>
2020-01-17 15:34:52 -0600

    se agrega diseño a la pagina de Home

--- src/view/home.js
+++ src/view/home.js
@@ -1,8 +1,21 @@
 export default () =>{
     const viewHome = `
-    <div class="logoNeek"><img src="./images/LogoNeek.png" alt="Logo neek" width="180"></div>
-    <h2><strong>NEEK</strong></h2>
-    <h4>Welcome to the social network from entrepreneurs</h4>
+        <div class='homePage'>
+
+            <div class='logoNeekHomePage'>
+                <img src="./images/LogoNeek.png" 
+                alt="Logo neek" 
+                width="145">
+            </div>
+
+            <div class='neekTextHomePage'>
+                <h2><strong>NEEK</strong></h2>
+            </div>
+
+            <div class='textSocialNHomePage'>
+                <h4>The social network for entrepreneurs</h4>
+            </div>
+        </div>
     `
     const divElement = document.createElement ('div');
     divElement.innerHTML = viewHome;
