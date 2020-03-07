fd5b9433f17aa3ddc3c83d5198153d0a94c1313a
erika-nath <enath6.25@gmail.com>
2020-03-06 19:37:56 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-import { example } from './example.js';
+//import { example } from './example.js';
 
-example();
+//example();
+
+
+//este archivo se va a inicializar cuando se recargue la pagina es nuestro controlador u orquestador
+// funcion init para cambiar la url de las rutas a donde va a dirigirse por cada vista
+//cada que escuches que hay una recarga ejecuta la funcion de init
+const init
