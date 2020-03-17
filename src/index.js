1b9fde132e0390cdd383bf514cf6ce84a7621c73
erika-nath <enath6.25@gmail.com>
2020-03-17 02:36:33 -0600

    esqueleto inputs y boton de login

--- src/index.js
+++
@@ -1,9 +0,0 @@
-//import { example } from './example.js';
-
-//example();
-
-
-//este archivo se va a inicializar cuando se recargue la pagina es nuestro controlador u orquestador
-// funcion init para cambiar la url de las rutas a donde va a dirigirse por cada vista
-//cada que escuches que hay una recarga ejecuta la funcion de init
-const init
---
+++ src/view/components.js
@@ -0,0 +1,14 @@
+import home from './sign-up.js' ;
+import login from './login.js';
+
+
+const components = {
+     home: home ,
+     login: login
+
+}
+
+export { components };
+
+
+//se crea un objeto con las vistas para este pasarlo a view controller
